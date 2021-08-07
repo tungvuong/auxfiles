3f21b3cf9287728534404d9491879bd91b4357a1
vuong <vuongt2@aalto.fi>
2021-08-07 11:51:56 +0300

    lesson1

---
+++ lesson1/tile_preview.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="tile_preview" tilewidth="32" tileheight="32" tilecount="15" columns="5">
+ <image source="../../Downloads/workadventure-tutorial-master/tile_preview.png" width="160" height="96"/>
+</tileset>
