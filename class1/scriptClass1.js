acef12851f1a5cdca6cfdf305fef2576c0e744c3
vuong <vuongt2@aalto.fi>
2021-08-09 20:14:13 +0300

    class1

---
+++ class1/scriptClass1.js
@@ -0,0 +1,24 @@
+var isFirstTimeTuto = false;
+var textFirstPopup = 'Hey ! This is how to start a discussion with someone ! You can be 4 max in a bubble.';
+var textSecondPopup = 'You can also use the chat to communicate ! ';
+var targetObjectTutoBubble ='Tutobubble';
+var targetObjectTutoChat ='tutoChat';
+var targetObjectTutoExplanation ='tutoExplanation';
+var popUpExplanation = undefined;
+var enterSoundUrl = "webrtc-in.mp3";
+var exitSoundUrl = "webrtc-out.mp3";
+var soundConfig = {
+    volume : 0.2,
+    loop : false
+}
+
+
+WA.onEnterZone('popupFrontZone', () => {
+    WA.loadSound(enterSoundUrl).play(soundConfig);
+    WA.displayBubble();
+});
+
+WA.onLeaveZone('popupFrontZone', () => {
+    WA.removeBubble();
+    WA.loadSound(exitSoundUrl).play(soundConfig);
+})
