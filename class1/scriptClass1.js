f65fc69c32996448443837852286d25370131035
vuong <vuongt2@aalto.fi>
2021-08-12 22:56:59 +0300

    class1 1_2 1_3

--- class1/scriptClass1.js
+++ class1/scriptClass1.js
@@ -32,7 +32,7 @@ var isAgentTalking = false;
 var isEngaged = false;
 function agent(project_id, session_id, text){
     isAgentTalking = true;
-    fetch('https://localhost:5000/bot', {
+    fetch('https://localhost:5000/bot/'+project_id, {
       method: 'post',
       headers: {
         'Accept': 'application/json, text/plain, */*',
@@ -111,7 +111,7 @@ WA.onEnterZone('popupReceptionZone', () => {
     WA.nav.openCoWebSite(htmlHost+"/1_1",false,"microphone");
     WA.displayBubble();
 
-    agent("welcome-rgfs", "abc", "hello");
+    agent("welcome_rgfs", "abc", "hello");
     isEngaged = true;
     // WA.nav.openCoWebSite("https://www.youtube.com/embed/BGSghRuCDJI?autoplay=1&muted=0",false,"autoplay");
     WA.nav.openCoWebSite("https://localhost/girltalk/tenor.gif",false,"microphone");
@@ -131,7 +131,7 @@ WA.onEnterZone('popupReceptionZone', () => {
                  ans = event.results[i][0].transcript;
              }
         }
-        agent("welcome-rgfs", "abc", ans);
+        agent("welcome_rgfs", "abc", ans);
         //console.log(res);
     };
 });
@@ -157,7 +157,7 @@ WA.onLeaveZone('popupBookExerciseZone', () => {
 })
 
 
-// ALEX EVENT
+// BOOK TEACHER EVENT
 WA.onEnterZone('popupBookTeacherZone', () => {
     WA.nav.openCoWebSite(htmlHost+"/1_3",false,"microphone");
     family_track = WA.loadSound('family_track.mp3');
@@ -171,3 +171,40 @@ WA.onLeaveZone('popupBookTeacherZone', () => {
     if (family_track !== undefined) family_track.stop();
 })
 
+// TEACHER EVENT
+WA.onEnterZone('popupTeacherZone', () => {
+    WA.nav.openCoWebSite(htmlHost+"/1_1",false,"microphone");
+    WA.displayBubble();
+
+    agent("family_agent", "abc", "hello");
+    isEngaged = true;
+    // WA.nav.openCoWebSite("https://www.youtube.com/embed/BGSghRuCDJI?autoplay=1&muted=0",false,"autoplay");
+    WA.nav.openCoWebSite("https://localhost/girltalk/tenor.gif",false,"microphone");
+    
+    if (isEngaged && !isAgentTalking) try{mic.start();} catch(e){mic.stop();}
+    mic.onstart = function() { 
+        console.log('speak');
+    };
+
+    mic.onerror = function(e) { console.log(e); };
+    mic.onend = function() { console.log('end'); if(isEngaged && !isAgentTalking) try{mic.start();} catch(e){mic.stop();} };
+    mic.onresult = function(event) {
+        ans = ""
+        for (var i = event.resultIndex; i< event.results.length; ++i) {
+            if (event.results[i].isFinal){
+                 console.log(event.results[i][0].transcript);
+                 ans = event.results[i][0].transcript;
+             }
+        }
+        agent("family_agent", "abc", ans);
+        //console.log(res);
+    };
+});
+
+WA.onLeaveZone('popupTeacherZone', () => {
+    mic.stop();
+    isEngaged = false;
+    if (speech !== undefined) window.speechSynthesis.cancel();
+    WA.removeBubble();
+    WA.nav.closeCoWebSite();
+})
