f1ab75a8fcd8983111f1b540c163a153d11d6c1b
vuong <vuongt2@aalto.fi>
2021-08-13 10:33:30 +0300

    class1 1_4

--- class1/scriptClass1.js
+++ class1/scriptClass1.js
@@ -212,3 +212,56 @@ WA.onLeaveZone('popupTeacherZone', () => {
 
 
 
+// book Alex EVENT
+WA.onEnterZone('popupBookAlexZone', () => {
+    WA.nav.openCoWebSite(htmlHost+"/1_4",false,"microphone");
+    family_track = WA.loadSound('greeting_track.mp3');
+    family_track.play({volume : 1,loop : false});
+    WA.displayBubble();
+});
+
+WA.onLeaveZone('popupBookAlexZone', () => {
+    WA.removeBubble();
+    WA.nav.closeCoWebSite();
+    if (family_track !== undefined) family_track.stop();
+})
+
+
+
+// alex EVENT
+WA.onEnterZone('popupAlexZone', () => {
+    WA.nav.openCoWebSite(htmlHost+"/1_4",false,"microphone");
+    WA.displayBubble();
+
+    agent("greeting-afky", "abc", "hello");
+    isEngaged = true;
+    // WA.nav.openCoWebSite("https://www.youtube.com/embed/BGSghRuCDJI?autoplay=1&muted=0",false,"autoplay");
+    // WA.nav.openCoWebSite("https://localhost/girltalk/boy.gif",false,"microphone");
+
+    if (isEngaged && !isAgentTalking) try{mic.start();} catch(e){mic.stop();}
+    mic.onstart = function() { 
+        console.log('speak');
+    };
+
+    mic.onerror = function(e) { console.log(e); };
+    mic.onend = function() { console.log('end'); if(isEngaged && !isAgentTalking) try{mic.start();} catch(e){mic.stop();} };
+    mic.onresult = function(event) {
+        ans = ""
+        for (var i = event.resultIndex; i< event.results.length; ++i) {
+            if (event.results[i].isFinal){
+                 console.log(event.results[i][0].transcript);
+                 ans = event.results[i][0].transcript;
+             }
+        }
+        agent("greeting-afky", "abc", ans);
+        //console.log(res);
+    };
+});
+
+WA.onLeaveZone('popupAlexZone', () => {
+    mic.stop();
+    isEngaged = false;
+    if (speech !== undefined) window.speechSynthesis.cancel();
+    WA.removeBubble();
+    WA.nav.closeCoWebSite();
+})
